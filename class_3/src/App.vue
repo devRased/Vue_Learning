02eae279d582ef5d06a58f0a7707ba747c616f7a
devRased <mdrasedulislam2525@gmail.com>
2023-11-29 09:59:09 +0600

    Markup div changed and add template tag which is support only vue.js

--- class_3/src/App.vue
+++ class_3/src/App.vue
@@ -9,35 +9,37 @@ const Weather = "s";
         <h1 class="text-center text-2xl py-10">{{ project_title }}</h1>
         <div class="w-1/3 bg-gray-200 p-10 flex items-center flex-col space-y-10">
             <h1 class="text-3xl">{{ location }}</h1>
-            <div class="flex items-center flex-col">
+            
+            <!-- make it group using div but div is shown on html murkup so we can use template tag which is support only vue.js but dont show in html murkup -->
+            <template v-if="Weather=='s'">
                 <h2 class="text-2xl" v-if="Weather == 's'">Weather: Sunny</h2>
                 <img
                     v-if="Weather == 's'"
                     src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7911203/weather-icon-md.png"
                     alt="sunny"
                 />
-            </div>
-            <div class="flex items-center flex-col">
+            </template>
+            <template v-if="Weather=='pc'">
                 <h2 class="text-2xl">Weather: Partly Cloudy</h2>
                 <img
                     src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912718/weather-icon-md.png"
                     alt="cloudy"
                 />
-            </div>
-            <div class="flex items-center flex-col">
+            </template>
+            <template v-if="Weather=='r'">
                 <h2 class="text-2xl">Weather: Rainy</h2>
                 <img
                     src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7913380/weather-icon-md.png"
                     alt="rainy"
                 />
-            </div>
-            <div class="flex items-center flex-col">
+            </template>
+            <template v-if="Weather=='t'">
                 <h2 class="text-2xl">Weather: Thunderstorm</h2>
                 <img
                     src="https://creazilla-store.fra1.digitaloceanspaces.com/icons/7912589/weather-icon-md.png"
                     alt="thunderstorm"
                 />
-            </div>
+            </template>
         </div>
     </section>
 </template>
